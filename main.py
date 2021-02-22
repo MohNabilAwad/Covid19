a42250660af5573db74bcd2eddbac20e4adc50e7
MohNabilAwad <79405221+MohNabilAwad@users.noreply.github.com>
2021-02-22 14:50:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import bs4
 import json
 from ftplib import FTP
 import datetime
-import time
+import csv
 
-#https://www.youtube.com/watch?v=e22HHgFqdm0
+# https://www.youtube.com/watch?v=e22HHgFqdm0
 page = requests.get("https://www.worldometers.info/coronavirus/")
-soup=bs4.BeautifulSoup(page.text,'lxml')
+soup = bs4.BeautifulSoup(page.text,'lxml')
 
 table = soup.find('table',id="main_table_countries_today")
 
-headers= [heading.text.replace(",Other","") for heading in table.find_all('th')]
+headers = [heading.text.replace(",Other","") for heading in table.find_all('th')]
 table_rows = [row for row in table.find_all('tr')]
 
 results = [{headers[index]:cell.text for index,cell in enumerate(row.find_all('td'))} for row in table_rows]
-
-
 now = datetime.datetime.now()
+
+csv_file = open('ICU Beds.csv')
+reader = csv.reader(csv_file, delimiter=',')
+ICUBEDS = list(reader)
 
 data = {}
 data['Info'] = []
@@ -25,16 +27,29 @@ data['Info'].append({
     'Last Update': now.strftime("%Y-%m-%d %H:%M:%S")
                     })
 data['Covid19'] = []
-
-
+#print(results)
 for i in results:
-    if("Country" in i):
 
+    if("Country" in i):
+        #print(ICUBEDS[j][0])
         if(i["Country"] != "Total:" and i["#"]!=""):
+            if(i["ActiveCases"]=='N/A'):
+                i["ActiveCases"]= "0"
+            if(i["Population"]==' '):
+                i["Population"]= "1"
+            for j in range(0,219):
+                if (str(i["Country"]) == str(ICUBEDS[j][0])):
+                    icuBeds=ICUBEDS[j][1]
+            for j in range(0,219):
+                if (str(i["Country"]) == str(ICUBEDS[j][0])):
+                    CountryCode=ICUBEDS[j][2]
             data['Covid19'].append({
-               'Country': i["Country"].replace("\n",""),
-               'ActiveCases': i["ActiveCases"],
-                'Population': i["Population"]
+                'Country': i["Country"].replace("\n",""),
+                'CountryCode': CountryCode,
+                'ActiveCases': i["ActiveCases"],
+                'Population': str(i["Population"]).replace(" ",""),
+                'Active Cases per 100k': round(int(str(str(i["ActiveCases"]).replace(",","")).replace(" ",""))/(int(str(i["Population"]).replace(",",""))/100000),2),
+                'ICU Beds per 100k': float(icuBeds)
                 })
 
 with open('data.json', 'w') as outfile:
@@ -50,4 +65,3 @@ ftp.quit()
 
 print("Done " + now.strftime("%Y-%m-%d %H:%M:%S"))
 
-time.sleep(10)
